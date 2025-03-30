416db8d4130ef91a7e11cb8b77036f21b9d50ab3
Munkhjin Nyamdorj <nyamkamunhjin@gmail.com>
2025-03-31 02:00:38 +0800

    cleanups and updates

--- components/app-header.tsx
+++ components/app-header.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { AuthNav } from "@/components/auth-nav";
-import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 
 interface NavItem {
